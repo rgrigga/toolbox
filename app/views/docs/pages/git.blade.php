5822a9989b77ae04e77a03d9ebc42f254602b60a
Ryan Grissinger <ryan.grissinger@gmail.com>
2014-09-29 12:51:25 -0400

    travis, git page

---
+++ app/views/docs/pages/git.blade.php
@@ -0,0 +1,4 @@
+<h4>git</h4>
+<p>This is the git page.</p>
+<p>Here is a prezi i've been working on for an upcoming presentation:</p>
+<a href="http://prezi.com/igioslmvhnyl/?utm_campaign=share&utm_medium=copy">Feature Branch Workflow Presentation</a>
---
+++ app/views/projects/toolbox/index.blade.php
@@ -0,0 +1,5 @@
+<h1>ToolBox</h1>
+<p>This is the info page for the Toolbox project.</p>
+<p>You can edit it at <code>views/projects/toolbox/index</code>.</p>
+<p>This page, and others like it, can be called like this:</p>
+<pre><code>View::make('projects/toolbox/index',['foo'=>$mydata]);</code></pre>
