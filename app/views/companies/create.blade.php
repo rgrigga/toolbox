3697e995581f3ef7d0b007263dbeefe06a62b589
Ryan Grissinger <ryan.grissinger@gmail.com>
2014-10-05 14:37:26 -0400

    myform styles

--- app/views/companies/create.blade.php
+++ app/views/companies/create.blade.php
@@ -10,86 +10,96 @@
     <script src="/assets/js/less.js" type="text/javascript"></script>
 @stop
 
-@section('main')
+@section('content')
 <?php
 // die(var_dump($company));
 ?>
         @if ($errors->any())
             <div class="row alert-error">
-            <ul>
-                {{ implode('', $errors->all('<li class="error">:message</li>')) }}
-            </ul>
+                <ul>
+                    {{ implode('', $errors->all('<li class="error">:message</li>')) }}
+                </ul>
             </div>
-
         @endif
+<div class="myformwrapper">
+    <h1>Create Company/Brand</h1>
 
+    {{ Form::open(array('route' => 'companies.store','class'=>'form-horizontal myform')) }}
+
+
+    <ul class="myform">
+        <li>
+            <div class="form-group">
+                {{ Form::label('brand', 'Brand:') }}
+                {{ Form::text('brand',null,['placeholder'=>'megacorp']) }}
+            </div>
+        </li>
+        <li>
+            <div class="form-group">
+                {{ Form::label('name', 'Name:') }}
+                {{ Form::text('name',null,['placeholder'=>'The Mega Corporation']) }}
+            </div>
+        </li>
+        <li>
+            <div class="form-group">
+                {{ Form::label('phone', 'Phone:') }}
+                {{ Form::text('phone',null,['placeholder'=>'614.555.1212']) }}
+            </div>
+        </li>
+        <li>
+            <div class="form-group">
+                {{ Form::label('email', 'Email:') }}
+                {{ Form::text('email',null,['placeholder'=>'megacorp@gmail.com']) }}
+            </div>
+        </li>
+        <li>
+            <div class="form-group">
+                {{ Form::label('description', 'Description:') }}
+                {{ Form::textarea('description',null,['placeholder'=>'Megacorp is awesome.']) }}
+            </div>
+        </li>
+        <li>
+            <div class="form-group">
+                {{ Form::label('slogan', 'Slogan:') }}
+                {{ Form::text('slogan',null,['placeholder'=>'Megacorp Rocks!']) }}
+            </div>
+        </li>
+        <li>
+            <div class="form-group">
+                <span>coming soon: avatar</span>
+                {{--Form::label('avatar','Logo:')--}}
+                {{--Form::file('avatar',null,['placeholder'=>'megacorp.png'])--}}
+            </div>
+        </li>
+        <li>
+            <div class="form-group">
+                {{ Form::label('menus', 'Menus:') }}
+                {{ Form::text('menus',null,['placeholder'=>'about,demo,help']) }}
+            </div>
+        </li>
+        <li>
+            <div class="form-group">
+                {{ Form::label('submit', 'Commit changes:') }}
+                {{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}
+            </div>
+        </li>
+
+    </ul>
+
+    {{ Form::close() }}
+
+</div>
 <div class="row">
-    <div class="span4">
-        <h1>Create Company/Brand</h1>
 
-        {{ Form::open(array('route' => 'companies.store')) }}
-            <ul>
-                <li>
-                    {{ Form::label('brand', 'Brand:') }}
-                    {{ Form::text('brand',null,['placeholder'=>'megacorp']) }}
-                </li>
-
-                <li>
-                    {{ Form::label('name', 'Name:') }}
-                    {{ Form::text('name',null,['placeholder'=>'The Mega Corporation']) }}
-                </li>
-
-                <li>
-                    {{ Form::label('phone', 'Phone:') }}
-                    {{ Form::text('phone',null,['placeholder'=>'614.555.1212']) }}
-                </li>
-
-                <li>
-                    {{ Form::label('email', 'Email:') }}
-                    {{ Form::text('email',null,['placeholder'=>'megacorp@gmail.com']) }}
-                </li>
-
-                <li>
-                    {{ Form::label('description', 'Description:') }}
-                    {{ Form::textarea('description',null,['placeholder'=>'Megacorp is awesome.']) }}
-                </li>
-
-                <li>
-                    {{ Form::label('slogan', 'Slogan:') }}
-                    {{ Form::text('slogan',null,['placeholder'=>'Megacorp Rocks!']) }}
-                </li>
-
-
-                <li>
-                    <span>coming soon: avatar</span>
-                    {{--Form::label('avatar','Logo:')--}}
-                    {{--Form::file('avatar',null,['placeholder'=>'megacorp.png'])--}}
-                </li>
-                <li>
-                    {{ Form::label('menus', 'Menus:') }}
-                    {{ Form::text('menus',null,['placeholder'=>'about,demo,help']) }}
-                </li>
-
-
-
-                <li>
-                    {{ Form::submit('Submit', array('class' => 'btn')) }}
-                </li>
-                <li>todo:
-                    <ul>
-                        <li>website</li>
-                        <li>address</li>
-                        <li>Gravatar::image('thomaswelton@me.com')</li>
-                        <li><a href="https://github.com/thomaswelton/laravel-gravatar">https://github.com/thomaswelton/laravel-gravatar</a></li>
-                    </ul>
-                </li>
-            </ul>
-        {{ Form::close() }}
-
-
-    </div>
     <div class="span7">
         <h2>Ideas:</h2>
+        <h3>todo:</h3>
+        <ul>
+            <li>website</li>
+            <li>address</li>
+            <li>Gravatar::image('thomaswelton@me.com')</li>
+            <li><a href="https://github.com/thomaswelton/laravel-gravatar">https://github.com/thomaswelton/laravel-gravatar</a></li>
+        </ul>
 <pre>
     Script here to create the neccesary folders.
     Or, perhaps a CLI tool to generate a Company
